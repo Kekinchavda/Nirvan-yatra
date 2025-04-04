cbb22c5fc56126402103a6ba6cfbfb9c8fd9d3b7
Kekin <kekinchavda@gmail.com>
2025-04-04 20:56:10 +0530

    - commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LocationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+route::get('/', [HomeController::class, 'index']);
+route::get('pannel', [HomeController::class, 'pannelLayoutIndex']);
+
+// location 
+route::get('location', [LocationController::class, 'index']);
+route::get('location/create-update', [LocationController::class, 'create']);
+route::any('location/create-or-update', [LocationController::class, 'createOrUpdate'])->name('createOrUpdate');
